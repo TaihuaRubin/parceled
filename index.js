ac88a41cc038190d8eab53f6fc2bdc93fadde504
Taihua <60823180+TaihuaRubin@users.noreply.github.com>
2021-05-19 15:09:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 (async function widget () {
-  const data = await fetch('https://resilia-review-pr-533.herokuapp.com/nonprofit/1').then(res=> res.json())
-
-  const container = document.querySelector('#resilia-container')
-  
-  const el = container?.append('MY NAME IS TAIHUA')
+  const data = await fetch('https://resilia-review-pr-533.herokuapp.com/nonprofit/1')
+  document.getElementById('root').apend(data)
 })()
